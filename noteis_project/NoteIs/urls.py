9a21d416b22c47c7b281fc03bbf501835823f68c
asliicelik <asliche1410@gmail.com>
2024-05-22 23:57:53 +0300

    Change titles and styling on homepage

--- noteis_project/NoteIs/urls.py
+++ noteis_project/NoteIs/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.home),
-    path('login/', views.login_page, name='login'),
-    path('register/', views.register_page, name='register'),
+    path('', views.home,name='home'),
+    path('login/', views.login_page_view, name='login'),
+    path('register/', views.register_page_view, name='register'),
 ]
--- noteis_project/NoteIs/views.py
+++ noteis_project/NoteIs/views.py
@@ -15,11 +15,40 @@ data =Database()
 def home(request):
     return render(request,"noteisapp/home.html")
 
-def login_page(request):
-    return render(request, 'login.html')
+def login_page_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
 
-def register_page(request):
-    return render(request, 'register.html')
+            if user:
+                request.session['uid'] = user['localId']
+                return render(request, 'noteisapp/home.html')
+            else:
+                error_message = "Geçersiz email veya şifre. Lütfen tekrar deneyin."
+                return render(request, 'noteisapp/login.html', {'error_message': error_message})
+
+        except Exception as e:
+            error_message = "Bir hata oluştu: {}".format(str(e))
+            return render(request, 'noteisapp/login.html', {'error_message': error_message})
+    else:
+        return render(request, 'noteisapp/login.html')
+
+def register_page_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            data.register(email, password)
+            return render(request, 'noteisapp/home.html')
+        except Exception as e:
+            error_message = "Bir hata oluştu: {}".format(str(e))
+            return render(request, 'noteisapp/register.html', {'error_message': error_message})
+    else:
+        return render(request, 'noteisapp/register.html')
+    
 
 
 def login_page(request):
